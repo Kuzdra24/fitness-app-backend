2e4dc555168f1c7a2f7d3636cf26f81ba67834a3
Patryk <patryk.kuzdrowski@gmail.com>
2022-11-29 12:06:52 +0100

    New API methods

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -1,24 +1,31 @@
 const Workout = require("../db/models/workout");
 
+//POST Create Workout
 const saveWorkout = async (req, res) => {
   const name = req.body.name;
   const target = req.body.target;
   const exercises = req.body.exercises;
   const newWorkout = new Workout({ name, target, exercises });
 
-  await newWorkout.save().then(() => {
-    console.log("Saved the workout");
-  });
+  try {
+    await newWorkout.save().then(() => {
+      console.log(`Saved the workout => ${JSON.stringify(newWorkout)}`);
+    });
+  } catch (err) {
+    return res.status(422).json({ message: err.message });
+  }
 
-  res.send(`Trening: ${name}  -  target: ${target}`);
+  res.status(201).json(newWorkout);
 };
 
+//GET Get all Workouts
 const getAllWorkout = async (req, res) => {
   const workouts = await Workout.find({});
   res.status(200).json(workouts);
 };
 
-const updateWorkout = async (req, res) => {
+//PUT Adding exercise into priticular workout
+const addExercise = async (req, res) => {
   const id = req.params.id;
   const newExc = req.body.exercise;
 
@@ -26,12 +33,32 @@ const updateWorkout = async (req, res) => {
   workout.exercises.push(newExc);
   await workout.save();
 
-  console.log('added workout')
+  console.log(`added workout ${JSON.stringify(newExc)}`);
   res.status(201).json(workout);
+};
+//PUT delete exercise from priticular workout
+const deleteExercise = async (req, res) => {
+  const id = req.params.id;
+  const choosedName = req.body.name;
+  
+  const workout = await Workout.findOne({ _id: id });
+  workout.exercises.filter(({ name }) => name !== choosedName);
+  
+  await workout.save();
+  res.status(201).json(workout);
+};
+
+//DELETE delete entire workout by ID
+const deleteWorkout = async (req, res) => {
+  const id = req.params.id;
+  await Workout.deleteOne({ _id: id });
+  res.status(204).send("usuniete bro");
 };
 
 module.exports = {
   saveWorkout,
   getAllWorkout,
-  updateWorkout,
+  addExercise,
+  deleteWorkout,
+  deleteExercise,
 };
--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const workoutAction = require("../controllers/workouts");
+const workoutController = require("../controllers/workouts");
 
-router.get("/workouts", workoutAction.getAllWorkout);
+router.get("/workouts", workoutController.getAllWorkout);
 
 // router.get("/workouts/:id", getWorkout);
 
-//Adding new exercisie into priticular workout 
-router.put("/workouts/:id", workoutAction.updateWorkout)
+//Adding new exercisie into priticular workout
+router.put("/workouts/add/:id", workoutController.addExercise);
+//Delete priticular exercises
+router.put('/workouts/delete/:id', workoutController.deleteExercise);
 
-router.post("/workouts", workoutAction.saveWorkout);
+router.post("/workouts", workoutController.saveWorkout);
 
-// router.delete("/workouts", deleteWorkout);
+router.delete("/workouts/:id", workoutController.deleteWorkout);
 
 module.exports = router;
